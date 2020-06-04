828d54946fd33e27ef51d56bfbcb61fb0ed67c9c
yugo <yugopolyp@gmail.com>
2020-06-05 00:09:51 +0900

    Modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,12 +36,18 @@ public class IndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
-        List<Task> messages = em.createNamedQuery("getAllMessages", Task.class)
+        List<Task> tasks = em.createNamedQuery("getAllMessages", Task.class)
                 .getResultList();
 
 
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
         em.close();
+
+
+         request.setAttribute("tasks", tasks);
+
+         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
+
     }
 
 }
--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
-
-
 
 /**
  * Servlet implementation class NewServlet
@@ -34,27 +30,15 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
 
-        // Messageのインスタンスを生成
-        Task m = new Task();
+        request.setAttribute("_token", request.getSession().getId());
 
-        // mの各プロパティにデータを代入
-        String content = "野球";
-        m.setContent(content);
 
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
+        request.setAttribute("task", new Task());
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
 
-        // データベースに保存
 
-        em.persist(m);
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-        em.close();
     }
 
 }
